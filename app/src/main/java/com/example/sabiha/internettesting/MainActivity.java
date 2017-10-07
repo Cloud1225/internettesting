47f66abbbb62c00031db05fde28f4135acbbda4a
sabiha <sabiha@ipvisionsoft.com>
2017-10-07 16:41:45 +0600

    Internet Checking

--- app/src/main/java/com/example/sabiha/internettesting/MainActivity.java
+++ app/src/main/java/com/example/sabiha/internettesting/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.sabiha.internettesting;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.support.v4.media.VolumeProviderCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean internetConnection = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(MainActivity.CONNECTIVITY_SERVICE);
+        NetworkInfo nf = cm.getActiveNetworkInfo();
+        if(nf.isConnected())
+        {
+            Toast.makeText(this, "Connected with " + nf.getExtraInfo() + " network", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "No network connection is available", Toast.LENGTH_LONG).show();
+        }
+
         checkInternetConnection ci = new checkInternetConnection();
         ci.execute();
     }
@@ -24,15 +42,36 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... params) {
             try {
                 InetAddress ia = InetAddress.getByName("www.google.com");
-                boolean test = !ia.equals("");
-                Log.e("Testing", "Connection: " + test);
+                InetAddress ia1 = InetAddress.getByName("www.amazon.com");
+                InetAddress ia2 = InetAddress.getByName("www.facebook.com");
+                internetConnection = !ia.equals("") || !ia1.equals("") || !ia2.equals("");
+                Log.e("Testing", "Connection: " + internetConnection);
             }
             catch(Exception e)
             {
                 Log.e("Testing", e.toString());
+                internetConnection = false;
+            }
+            finally {
+                createNotification();
             }
             return null;
         }
+    }
+
+    private void createNotification()
+    {
+        Resources res = this.getResources();
+        int notificationID = 1;
+        NotificationCompat.Builder ncBuilder = new NotificationCompat.Builder(MainActivity.this);
+        ncBuilder.setAutoCancel(true)
+            .setContentTitle(res.getString(R.string.internet_connection_title))
+            .setContentText(internetConnection ? res.getString(R.string.internet_connection_true)
+                    : res.getString(R.string.internet_connection_false))
+            .setSmallIcon(R.drawable.ic_network_check_white_24dp);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(notificationID, ncBuilder.build());
     }
 
     /*public static boolean isConnected(Context context) {
